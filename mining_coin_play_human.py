98463a9659124c29e5649412fddf8f3e7d7e27ea
MinzhiYoyo <2498369702@qq.com>
2024-01-28 14:45:50 +0800

    GUI is ok!

--- mining_coin_play_human.py
+++ mining_coin_play_human.py
@@ -6,22 +6,24 @@ env = miningcoin.MiningCoinEnv()
 env.reset()
 env_ui = gameui.GameUI(env)
 info = None
-while env.done is False:
-    if not info:
-        print(info)
+
+while True:
+    if info:
+        print('info is {}.'.format(info))
     action = env_ui.draw(info=info)
 
-    if not action:
-        print('no action')
-    elif action < 0:
-        print('quit game')
-        break
-    elif action in miningcoin.ACTION:
-        print('action: {}'.format(action))
-        _, _, _, info = env.step(action)
-    else:
-        env.reset()
-        info = None
-        env_ui.reset()
-        print('reset the game')
+    info = None
+    if action is not None: 
+        print('action is {}.'.format(action))
+        if action in miningcoin.ACTION:
+            _, _, _, info = env.step(action)
+        elif action < 0:
+            print('Game over.')
+            break
+        else:
+            env.reset()
+            info = None
+            env_ui.reset()
+            print('reset the game.')
+    
 env_ui.quit()
